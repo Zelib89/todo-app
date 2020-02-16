7bffee1b9cfca8b6bf17de17e2e3afed49b490b1
Ruslan Kurtianyk <ruslan.kurtjanyk@gmail.com>
2020-02-16 20:40:43 +0200

    refactor: add bprod build to git

---
+++ client/build/precache-manifest.975645aad59538089d5040d3c1aba9d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a346afc2a440a075b257855e1d19cfba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5b84735ec6a433e205a",
+    "url": "/static/css/main.09e8a2d0.chunk.css"
+  },
+  {
+    "revision": "a3ea06ab1806f70d33bb",
+    "url": "/static/js/2.9b76ddef.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/static/js/2.9b76ddef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5b84735ec6a433e205a",
+    "url": "/static/js/main.13ac9ec4.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
