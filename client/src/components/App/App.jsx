ce67fb22cf816b239bd6c9e01e7c234220be9a21
rkurtianyk <rkurtjanyk@prismhr.com>
2020-02-18 15:11:55 +0200

    refactor: fix lint errors

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, Link } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import { history } from '../../utils/history';
 import { authService } from '../../services/auth';
 import PrivateRoute from '../PrivateRoute';
